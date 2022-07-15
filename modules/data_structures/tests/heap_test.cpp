ecc52bf9ad6d821f517f60dea21921589f36a19e
jens <jens@doellmann.com>
2022-07-16 01:33:40 +0200

    rename

---
+++ modules/data_structures/tests/heap_test.cpp
@@ -0,0 +1,3 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Foo", "[heap]") { REQUIRE(1 == 1); }
--- modules/data_structures/tests/linked_list_test.cpp
+++ modules/data_structures/tests/linked_list_test.cpp
@@ -1,3 +1,8 @@
+#include "linked_list.hpp"
+
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Factorials are computed", "[factorial]") { REQUIRE(1 == 1); }
+TEST_CASE("My first test", "[linked_list]") {
+  ds::LinkedList<int> list{};
+  REQUIRE(list.count() == 0);
+}
